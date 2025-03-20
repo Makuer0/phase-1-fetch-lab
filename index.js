0b7b18f7a06ccc24c9faf6d6de2aefa61e2db2cb
Makuer <makuer.mqtur@student.moringaschool.com>
2025-03-20 14:19:27 +0300

    fetch labs

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+ return fetch('https://anapioficeandfire.com/api/books')
+  .then(Response => Response.json())
+  .then(data => renderBooks(data))
+
+
 }
 
 function renderBooks(books) {
